5f34f0241c69957b6ad55085b94e94a8d290994b
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-27 16:01:12 +0530

    commit matplotlib

--- source/rmsprop.py
+++ source/rmsprop.py
@@ -3,3 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# creating the output array
+y=np.array([[1],[1],[0]])
+
+# output in matrix form
+y = y.T
+
+print ('\n Actual Output:')
+print(y)
+
+# shape of output array
+print('\n Shape of Output:', y.shape)
+
+
